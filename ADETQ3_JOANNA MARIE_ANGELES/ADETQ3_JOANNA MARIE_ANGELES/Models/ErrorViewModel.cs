170c24a2d3fc18805db6f4dd9a45bd101d7827b8
JOANNA MARIE ANGELES <1146-18@itmlyceumalabang.onmicrosoft.com>
2020-10-24 02:57:39 +0800

    Angeles Joanna Marie

---
+++ ADETQ3_JOANNA MARIE_ANGELES/ADETQ3_JOANNA MARIE_ANGELES/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADETQ3_JOANNA_MARIE_ANGELES.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
